aa3d4759740821a7dadb1ffea50f08cdc15ec267
drag0nD <abdullah.radidaa@gmail.com>
2025-02-04 18:26:19 +0300

    Update index.js

--- bp/scripts/index.js
+++ bp/scripts/index.js
@@ -5,15 +5,17 @@ world.afterEvents.chatSend.subscribe(({ message, sender: player }) => {
     const form = new ABF()
         .title('   What option do you think is correct?    \n')
         .pattern([
-            ['#          ', '%1 0_10', "#                    ", "%0"],
+            ['#          ', '%1 0_10', "#                    ", "%0 def_def 100"],
             ['#      ', 'Option 3', '#      ', 'Option 4', '#\n'],
             ['#      ', '#Do you think this form is cool?', '#      '],
             ['#         ', 'Yes', '#            ', 'No', '#'],
-
+            ['#      ', '#Do you think this form is cool?', '#      '],
+            ['#         ', 'Yes', '#            ', 'No', '#']
         ])
         .scripts(Scripts.basic)
     form.ui = UI.darkMode
-    form.settings.type.default();
+    form.settings.increament = 2.5
+    form.settings.type.slow();
 
     form.offset(30,30)
     form.show(player, 0, 1).then(({ line, slot, cancelled }) => {
