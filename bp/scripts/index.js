3990edcb094f406b0bbfe43da8538ccabef6a0c4
drag0nD <abdullah.radidaa@gmail.com>
2025-02-05 15:34:56 +0300

    Update index.js

--- bp/scripts/index.js
+++ bp/scripts/index.js
@@ -2,22 +2,25 @@ import { world } from "@minecraft/server"
 import {ABF,UI,Scripts} from "abf.js";
 world.afterEvents.chatSend.subscribe(({ message, sender: player }) => {
     if (message != 'form') return;
+    // number input syntax: "%<default value> <min_max> <increament>"  min_max values and increaments are optional, you can set global values in settings
+    // toggle syntax: "$<default value (true/false)>"
     const form = new ABF()
         .title('   What option do you think is correct?    \n')
         .pattern([
-            ['#          ', '%1', "#                    ", "%0 def_def 100"],// number input syntax: "%<default value> <min_max> <increament>"  min_max values and increaments are optional, you can set global values in settings
-            ['#      ', 'Option 3', '#      ', 'Option 4', '#\n'],
-            ['#      ', '#Do you think this form is cool?', '#      '],
-            ['#         ', 'Yes', '#            ', 'No', '#']
+            ['#      ', 'option 1', "#      ", "option 2"],
+            ['#      ', 'Option 3', '#      ', 'Option 4'],
+            ["#      ", "#Number example: ", "%0"],
+            ["#      ", "#Toggle 1: ", "$false"],
+            ["#      ", "#Toggle 2: ", "$true"]
         ])
         .scripts(Scripts.basic)
     form.ui = UI.darkMode
-    form.settings.type.slow();
+    form.settings.type.default();
 
     form.offset(30,30) //you can enable devmode in .show() to find the optimal offset for you
-    form.show(player, 0, 1).then(({ line, slot, cancelled, values }) => {
+    form.show(player, 0, 0).then(({ line, slot, cancelled, values }) => {
         if (cancelled) return;
 
-        player.sendMessage(JSON.stringify(values))
+        if (values) player.sendMessage(JSON.stringify(values))
     })
 });
